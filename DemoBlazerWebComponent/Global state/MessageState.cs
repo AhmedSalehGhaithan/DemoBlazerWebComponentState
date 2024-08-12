b3b0d89cf290427761e671df7b0ead5676cbf428
Ahmed ghaithan <ghaithanahmed691@gmail.com>
2024-08-13 01:18:21 +0300

    Add project files.

---
+++ DemoBlazerWebComponent/Global state/MessageState.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoBlazerWebComponent.Global_state
+{
+    public class MessageState
+    {
+        public string? Message { get; set; }
+        public string? Message2 { get; set; }
+
+        public void SetMessage(string message)
+        {
+            Message2 = message;
+        }
+        public void CleanMessage()
+        {
+            Message2 = null;
+        }
+    }
+}
